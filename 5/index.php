7d7947ccc430b1d8849a19c444e98b20d9542cf7
Ivan Chernomorov <90576997+IvanChernomorov@users.noreply.github.com>
2022-05-19 23:12:32 +0300

    Add files via upload

---
+++ 5/index.php
@@ -0,0 +1,204 @@
+<?php
+
+define("BASE_DIR", __DIR__ . DIRECTORY_SEPARATOR);
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	session_start();
+	if (!empty($_COOKIE['save'])) {
+		setcookie("save", '', time() - 60 * 60 * 24);
+		setcookie("login", '', time() - 60 * 60 * 24);
+		setcookie("password", '', time() - 60 * 60 * 24);
+		$fheader = "<p>Ваши данные сохранены!<p>";
+		//TODO: сделать логин
+	} elseif (!empty($_COOKIE['update'])) {
+		setcookie("update", '', time() - 60 * 60 * 24);
+		$fheader = "<p>Ваши данные обновлены!<p>";
+	} else {
+		$fheader = "<p>Заполните данные!<p>";
+	}
+	$message = array();
+	checkCookies('name', $message);
+	checkCookies('email', $message);
+	checkCookies('year', $message);
+	checkCookies('gender', $message);
+	checkCookies('numlimbs', $message);
+	checkCookies('super-powers', $message);
+	checkCookies('biography', $message);
+	require_once("form.php");
+	if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])) {
+		//TODO: Сделать выход
+	} else {
+		//TODO: Сделать возможность входа есди уже есть акк
+	}
+} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	session_start();
+	require_once(BASE_DIR . "src/UserData.php");
+	require_once(BASE_DIR . "src/formHandler.php");
+	$dbServerName = 'localhost';
+	$dbUser = "u47556";
+	$dbPassword = "2195834";
+	$dbName = $dbUser;
+
+	if (!empty($_POST)) {
+
+		$userData = new UserData(
+			$_POST['name'] ?? '',
+			$_POST['email'] ?? '',
+			$_POST['year'] ?? '',
+			$_POST['gender'] ?? '',
+			$_POST['numlimbs'] ?? '',
+			$_POST['super-powers'] ?? '',
+			$_POST['biography'] ?? '',
+		);
+
+		$errors = formHandler::checkUserData($userData);
+
+		writeCookies('name', $errors, $userData->getName());
+		writeCookies('email', $errors, $userData->getEmail());
+		writeCookies('year', $errors, $userData->getYear());
+		writeCookies('gender', $errors, $userData->getGender());
+		writeCookies('numlimbs', $errors, $userData->getNumlimbs());
+		writeCookies('biography', $errors, $userData->getBiography());
+
+		if (isset($errors['super-powers'])) {
+			setcookie('super-powers-error', $errors['super-powers'], time() + 60 * 60 * 24);
+		} else {
+			$supPowers = array('1' => '0', '2' => '0', '3' => '0');
+			foreach ($userData->getSuperPowers() as $value) {
+				$supPowers[$value] = '1';
+			}
+			foreach ($supPowers as $key => $value) {
+				setcookie("super-powers[$key]", $value, time() + 60 * 60 * 24 * 365);
+			}
+		}
+	}
+
+	if (count($errors) > 1) {
+		header("Location: index.php");
+		exit();
+	}
+
+	$db = new PDO("mysql:host=$dbServerName;dbname=$dbName", $dbUser, $dbPassword, array(PDO::ATTR_PERSISTENT => true));
+
+	if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])) {
+		$userId = intval($_SESSION['loginid']);
+
+		try {
+			$sql = "UPDATE user2 SET name = :name, email = :email, date = :date, gender = :gender, limbs = :limbs, biography = :biography WHERE id = :id";
+			$stmt = $db->prepare($sql);
+			$stmt->execute(array(
+				'id' => $userId, 'name' => $userData->getName(), 'email' => $userData->getEmail(),
+				'date' => intval($userData->getYear()), 'gender' => $userData->getGender(), 'limbs' => intval($userData->getNumlimbs()),
+				'biography' => $userData->getBiography()
+			));
+		} catch (PDOException $e) {
+			print('Error : ' . $e->getMessage());
+			exit();
+		}
+
+		try {
+			$sql = "DELETE FROM user_power2 WHERE id = :id";
+			$stmt = $db->prepare($sql);
+			$stmt->execute(array('id' => $userId));
+		} catch (PDOException $e) {
+			print('Error : ' . $e->getMessage());
+			exit();
+		}
+
+		try {
+			foreach ($userData->getSuperPowers() as $value) {
+				$stmt = $db->prepare("INSERT INTO user_power2 (id, power) VALUES (:id, :power)");
+				$stmt->execute(array('id' => $userId, 'power' => intval($value)));
+			}
+		} catch (PDOException $e) {
+			print('Error : ' . $e->getMessage());
+			exit();
+		}
+		setcookie("update", '1', time() + 60 * 60 * 24);
+	} else {
+		$lastId = null;
+
+		try {
+			$stmt = $db->prepare("INSERT INTO user2	(name, email, date, gender, limbs, biography) 
+				VALUES (:name, :email, :date, :gender, :limbs, :biography)");
+
+			$stmt->execute(array(
+				'name' => $userData->getName(), 'email' => $userData->getEmail(), 'date' => intval($userData->getYear()),
+				'gender' => $userData->getGender(), 'limbs' => intval($userData->getNumlimbs()),
+				'biography' => $userData->getBiography()
+			));
+
+			$lastId = $db->lastInsertId();
+		} catch (PDOException $e) {
+			print('Error : ' . $e->getMessage());
+			exit();
+		}
+
+		try {
+			if ($lastId === null) {
+				exit();
+			}
+			foreach ($userData->getSuperPowers() as $value) {
+				$stmt = $db->prepare("INSERT INTO user_power2 (id, power) VALUES (:id, :power)");
+				$stmt->execute(array('id' => $lastId, 'power' => intval($value)));
+			}
+		} catch (PDOException $e) {
+			print('Error : ' . $e->getMessage());
+			exit();
+		}
+
+		$login =  "user$lastId";
+		$password = gen_password();
+		try {
+			$stmt = $db->prepare("INSERT INTO user_authentication (id, login, password) VALUES (:id, :login, :password)");
+			$stmt->execute(array('id' => $lastId, 'login' => $login, 'password' => password_hash($password, PASSWORD_DEFAULT)));
+		} catch (PDOException $e) {
+			print('Error : ' . $e->getMessage());
+			exit();
+		}
+
+		setcookie('login', $login, time() + 60 * 60 * 24);
+		setcookie('password', $password, time() + 60 * 60 * 24);
+		setcookie("save", '1', time() + 60 * 60 * 24);
+	}
+
+	$db = null;
+
+	header("Location: index.php");
+	exit();
+}
+
+function checkCookies($name, &$message)
+{
+	if (!empty($_COOKIE[$name])) {
+		$message[$name] = $_COOKIE[$name];
+	} else {
+		$message[$name] = '';
+	}
+	if (!empty($_COOKIE[$name . '-error'])) {
+		$message[$name . '-error'] = "<div class='error'>{$_COOKIE[$name . '-error']}</div>";
+		setcookie($name . '-error', '', time() - 60 * 60 * 24);
+	} else {
+		$message[$name . '-error'] = '';
+	}
+}
+function writeCookies($name, $errors, $userData)
+{
+	if (isset($errors[$name])) {
+		setcookie($name . '-error', $errors[$name], time() + 60 * 60 * 24);
+	} else {
+		setcookie($name, $userData, time() + 60 * 60 * 24 * 365);
+	}
+}
+function gen_password($length = 12)
+{
+	$chars = 'qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP';
+	$size = strlen($chars) - 1;
+	$password = '';
+
+	while ($length--) {
+		$password .= $chars[random_int(0, $size)];
+	}
+
+	return $password;
+}
