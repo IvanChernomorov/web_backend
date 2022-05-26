a2abfb86f790348aeea3be2116ce74837585afb7
Ivan Chernomorov <90576997+IvanChernomorov@users.noreply.github.com>
2022-05-27 01:41:42 +0300

    Update login.php

--- 5/login.php
+++ 5/login.php
@@ -1,14 +1,19 @@
 <?php
 
 define("BASE_DIR", __DIR__ . DIRECTORY_SEPARATOR);
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	session_start();
 
 	if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])) {
-		if(isset($_GET['do'])&&$_GET['do'] == 'logout'){
-			session_destroy();
-		}
+		session_destroy();
 		header("Location: index.php");
 		exit();
+	}
+
+	if (!empty($_COOKIE['login-request-error'])) {
+		setcookie("login-request-error", '', time() - 60 * 60 * 24);
+		$lheader  = "Что-то пошло не так!";
 	} elseif (!empty($_COOKIE['login-auth-error'])) {
 		setcookie('login-auth-error', '', time() - 60 * 60 * 24);
 		$lheader  = "Неверный логин и/или пароль";
@@ -30,10 +35,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 	}
 	require_once("loginpage.php");
 } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	session_start();
 	$dbServerName = 'localhost';
 	$dbUser = "u47556";
 	$dbPassword = "2195834";
 	$dbName = $dbUser;
+	$requestError = false;
 	if (!empty($_POST)) {
 		if (empty($_POST["login"])) {
 			$errors['login'] = "Введите логин";
@@ -42,50 +49,52 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 		if (empty($_POST["password"])) {
 			$errors['password'] = "Введите пароль";
 		}
-
-		if (isset($errors['login'])) {
-			setcookie('login-error', $errors['login'], time() + 60 * 60 * 24);
-		}
-		if (isset($errors['password'])) {
-			setcookie('password-error', $errors['password'], time() + 60 * 60 * 24);
-		}
-
-
-		if (isset($errors)) {
-			header("Location: login.php");
-			exit();
-		}
-
-		$userLogin = $_POST["login"];
-		$userPassword = $_POST["password"];
-		$db = new PDO("mysql:host=$dbServerName;dbname=$dbName", $dbUser, $dbPassword, array(PDO::ATTR_PERSISTENT => true));
-		$success = false;
-		try {
-			$sql =
-				"SELECT * FROM user_authentication
-			WHERE login = :login";
-			$stmt = $db->prepare($sql);
-			$stmt->execute(array('login' => $userLogin));
-			$result = $stmt->fetch();
-
-			if (!empty($result)) {
-				$success = password_verify($userPassword, $result['password']);
-				$userId = $result['id'];
-			}
-		} catch (PDOException $e) {
-			print('Error : ' . $e->getMessage());
-			exit();
-		}
-
-		if ($success) {
-			session_start();
-			$_SESSION['login'] = $userLogin;
-			$_SESSION['loginid'] = $userId;
-		} else {
-			setcookie('login-auth-error', '1', time() + 60 * 60 * 24);
-			header("Location: login.php");
-			exit();
-		}
-		header("Location: index.php");
+	} else {
+		$requestError = true;
 	}
+
+
+	if (isset($errors['login'])) {
+		setcookie('login-error', $errors['login'], time() + 60 * 60 * 24);
+	}
+	if (isset($errors['password'])) {
+		setcookie('password-error', $errors['password'], time() + 60 * 60 * 24);
+	}
+
+
+	if (isset($errors)) {
+		header("Location: login.php");
+		exit();
+	}
+
+	$userLogin = $_POST["login"];
+	$userPassword = $_POST["password"];
+	$db = new PDO("mysql:host=$dbServerName;dbname=$dbName", $dbUser, $dbPassword, array(PDO::ATTR_PERSISTENT => true));
+	$success = false;
+	try {
+		$sql =
+			"SELECT * FROM user_authentication
+			WHERE login = :login";
+		$stmt = $db->prepare($sql);
+		$stmt->execute(array('login' => $userLogin));
+		$result = $stmt->fetch();
+
+		if (!empty($result)) {
+			$success = password_verify($userPassword, $result['password']);
+			$userId = $result['id'];
+		}
+	} catch (PDOException $e) {
+		print('Error : ' . $e->getMessage());
+		exit();
+	}
+
+	if ($success) {
+		$_SESSION['login'] = $userLogin;
+		$_SESSION['loginid'] = $userId;
+	} else {
+		setcookie('login-auth-error', '1', time() + 60 * 60 * 24);
+		header("Location: login.php");
+		exit();
+	}
+	header("Location: index.php");
 }
