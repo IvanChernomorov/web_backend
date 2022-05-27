e4286b03669bfa6b6bc136d8820f1837323579a4
Ivan Chernomorov <90576997+IvanChernomorov@users.noreply.github.com>
2022-05-27 05:09:43 +0300

    Update adminlog.php

--- 6/adminlog.php
+++ 6/adminlog.php
@@ -37,6 +37,23 @@
 		}
 		?>
 		<table border="1" width="25%" cellpadding="5">
+	<tr>
+		<th>ID</th>
+		<th>Суперспособность</th>
+	</tr>
+	<tr>
+		<?php
+		foreach ($dbRequester->getSupPowUsersData() as $key => $value) {
+			echo "
+			<tr>
+				<td>" . htmlspecialchars($value['id']) . "</td>
+				<td>" . htmlspecialchars($value['power']) . "</td>
+			</tr>";
+		}
+		?>
+	</tr>
+</table>
+		<table border="1" width="25%" cellpadding="5">
 			<tr>
 				<th>Суперспособность</th>
 				<th>Количество</th>
